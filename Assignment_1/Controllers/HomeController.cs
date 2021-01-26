888e3a33bb8a6711d1c9f161f9457dbfefa15a18
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-01-26 11:03:05 -0700

    Assignment 2

---
+++ Assignment_1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Calculator")]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost("Calculator")]
+        public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment_1/Models/CalculatorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_1.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0,100)]
+        public int assignments { get; set; }
+        [Range(0, 100)]
+        public int group { get; set; }
+        [Range(0, 100)]
+        public int quiz { get; set; }
+        [Range(0, 100)]
+        public int exam { get; set; }
+        [Range(0, 100)]
+        public int intex { get; set; }
+    }
+}
